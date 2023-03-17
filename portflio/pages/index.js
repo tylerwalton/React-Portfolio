c12d73db9df4527b53043984d7ef6675ab59e85c
Tyler Walton <tylerwalton300@gmail.com>
2023-03-16 22:11:47 -0400

    initial commit with index.js set up

---
+++ portflio/pages/index.js
@@ -0,0 +1,30 @@
+import Head from "next/head";
+import About from "../components/About";
+import Contact from "../components/Contact";
+import Main from "../components/Main";
+import Projects from "../components/Projects";
+import Skills from "../components/Skills";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Tyler | Full-Stack Developer</title>
+        <meta
+          name="description"
+          content="I’m a full stack web developer specializing in Javascript, SQL, React, CSS, HTML and Node"
+        />
+        <link rel="icon" href="/fav.png" />
+      </Head>
+      <Main />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </div>
+  );
+}
+
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
