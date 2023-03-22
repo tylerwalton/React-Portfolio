f82455d30d0e4a5a3e5a9b5cbb02000c90b2eb19
Tyler Walton <tylerwalton300@gmail.com>
2023-03-21 21:42:27 -0400

    Adding styling to Navbar

--- portflio/components/Navbar.jsx
+++ portflio/components/Navbar.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import Link from 'next/link';
+import {AiOutlineClose} from 'react-icons/ai'
 
 const Navbar = () => {
   return (
-    <div>
-      <h2> This is a nav bar</h2>
+    <div className="fixed w-full h-30 shadow-xl z-[100]">
+      <div className="flex justify-between items-center w-full h-full px-2 sxl:px-16">
+        <Image src="/../public/assets/TW.png" alt="/" width="110" height="40" />
+        <div>
+          <ul className="hidden md:flex">
+              <Link href="/">
+                <li className="ml-10 text-sm uppercase hover:border-b">
+                  <Link href="/">Home</Link>
+                </li>
+                <li className="ml-10 text-sm uppercase hover:border-b">
+                  <Link href="/#about">About</Link>
+                </li>
+                <li className="ml-10 text-sm uppercase hover:border-b">
+                  <Link href="/#skills">Skills</Link>
+                </li>
+                <li className="ml-10 text-sm uppercase hover:border-b">
+                  <Link href="/#projects">Projects</Link>
+                </li>
+                <li className="ml-10 text-sm uppercase hover:border-b">
+                  <Link href="/resume">Resume</Link>
+                </li>
+                <li className="ml-10 text-sm uppercase hover:border-b">
+                  <Link href="/#contact">Contact</Link>
+                </li>
+              </Link>
+          </ul>
+          <div className="md:hidden">
+            <AiOutlineClose />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- portflio/pages/index.js
+++ portflio/pages/index.js
@@ -1,20 +1,20 @@
 import Head from "next/head";
+import React from "react";
+import Navbar from "../components/Navbar";
 // import About from "../components/About";
 // import Contact from "../components/Contact";
 // import Main from "../components/Main";
 // import Projects from "../components/Projects";
 // import Skills from "../components/Skills";
-import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
     <div> 
       <Head>
-          <title>Tyler | Full-Stack Developer</title>
+        <title>Tyler | Full-Stack Developer</title>
         <meta
           name="description"
-          content="I’m a full stack web developer specializing in Javascript, SQL, React, CSS, HTML and Node"
-        />
+          content="I’m a full stack web developer specializing in Javascript, SQL, React, CSS, HTML and Node"/>
         <link rel="icon" href="/fav.png" />
       </Head>
       <Navbar />
