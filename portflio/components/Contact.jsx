b691ae16661407337df12c1cb39795b2cd9ab9a1
Tyler Walton <tylerwalton300@gmail.com>
2023-03-21 20:22:24 -0400

    adding in routes to nav home and about

--- portflio/components/Contact.jsx
+++ portflio/components/Contact.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
     <div>
       <h2> This is a nav bar</h2>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- portflio/pages/index.js
+++ portflio/pages/index.js
@@ -1,9 +1,10 @@
 import Head from "next/head";
-import About from "../components/About";
-import Contact from "../components/Contact";
-import Main from "../components/Main";
-import Projects from "../components/Projects";
-import Skills from "../components/Skills";
+// import About from "../components/About";
+// import Contact from "../components/Contact";
+// import Main from "../components/Main";
+// import Projects from "../components/Projects";
+// import Skills from "../components/Skills";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
         />
         <link rel="icon" href="/fav.png" />
       </Head>
+      <Navbar />
     </div>
   )
 }
